6aec9f11627d8ac2a29d3ab472dde93555d21b33
mrKraft <mrkr.reg@gmail.com>
2010-11-30 20:49:49 +0300

    Начал писать класс AIPlayer - искуственный интелект ( игрок-компьютер ).

---
+++ AIPlayer.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+
+class AIPlayer {
+private:
+
+	
+
+public:
+	
+	AIPlayer() {
+
+	}
+
+	~AIPlayer() {
+	}
+
+	
+	
+};
